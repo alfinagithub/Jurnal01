3baae77abac4f53b49a4118470398a73d7a398fb
Alfina Azizah <alfinaazizah1@gmail.com>
2023-09-27 09:36:31 +0700

    commit pertama

---
+++ Jurnal01.java
@@ -0,0 +1,8 @@
+public class Jurnal01 {
+    
+    public static void main (String [] args) {
+        System.out.println("NAMA: ALFINA AZIZAH");
+        System.out.println("NIM: 607062330022");
+        System.out.println("KELAS: D3IF-47-04");
+    }
+}
